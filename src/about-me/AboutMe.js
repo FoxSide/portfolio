eac4c48d0ce9824e92584c6cc0989491adad834d
FoxSide <foxside8452@gmail.com>
2022-04-06 19:14:43 +0300

    uptade

--- src/about-me/AboutMe.js
+++ src/about-me/AboutMe.js
@@ -4,6 +4,7 @@ import photoMe from '../assets/image/photoMe.jpg'
 import telegramImg from '../assets/image/telegram.png'
 import githubImg from '../assets/image/github.png'
 import linkedImg from '../assets/image/linkedIn.png'
+import {Fade, Rotate, Zoom} from "react-awesome-reveal";
 
 const aboutMeText = 'I am a person who has a strong passion for programming. I have experience building a SPA with React/Redux/TypeScript. In my free time, I expand my knowledge in the field of front-end development, devote time to Codewars, and also improve my knowledge of the English language.'
 
@@ -12,29 +13,38 @@ const AboutMe = () => {
     <div className={s.mainBlock}>
       <div className={s.container}>
         <div className={s.text}>
-          <span className={s.subtitle}>Welcome to my world</span>
-          <h1 className={s.title}>Hi, I'm <span className={s.name}>Eduard Mysan</span>
-            <br/>a Front-end Developer
-          </h1>
-          <div>
-            <p className={s.description}>{aboutMeText}</p>
-          </div>
-          <div className={s.findContainer}>
-            <span className={s.contacText}>contact with me</span>
-            <div className={s.contactsGroup}>
-              <a href={'https://t.me/foxside'} target="_blank" className={s.contactContainer} rel="noreferrer">
-                <img src={telegramImg} alt="img"/>
-              </a>
-              <a href={'https://github.com/FoxSide'} target="_blank" className={s.contactContainer} rel="noreferrer">
-                <img src={githubImg} alt="img"/>
-              </a>
-              <a href={'https://www.linkedin.com/in/eduard-mysan/'} target="_blank" className={s.contactContainer} rel="noreferrer">
-                <img src={linkedImg} alt="img"/>
-              </a>
+          <Fade direction={'down'}>
+            <span className={s.subtitle}>Welcome to my world</span>
+          </Fade>
+          <Fade direction={'left'}>
+            <h1 className={s.title}>Hi, I'm <span className={s.name}>Eduard Mysan</span>
+              <br/>a Front-end Developer
+            </h1>
+            <div>
+              <p className={s.description}>{aboutMeText}</p>
             </div>
-          </div>
+          </Fade>
+          <Fade direction={'right'}>
+            <div className={s.findContainer}>
+              <span className={s.contacText}>contact with me</span>
+              <div className={s.contactsGroup}>
+                <a href={'https://t.me/foxside'} target="_blank" className={s.contactContainer} rel="noreferrer">
+                  <img src={telegramImg} alt="img"/>
+                </a>
+                <a href={'https://github.com/FoxSide'} target="_blank" className={s.contactContainer} rel="noreferrer">
+                  <img src={githubImg} alt="img"/>
+                </a>
+                <a href={'https://www.linkedin.com/in/eduard-mysan/'} target="_blank" className={s.contactContainer}
+                   rel="noreferrer">
+                  <img src={linkedImg} alt="img"/>
+                </a>
+              </div>
+            </div>
+          </Fade>
         </div>
-        <img className={s.photoMe} src={photoMe} alt="photoMe"/>
+        <Fade direction={'right'}>
+          <img className={s.photoMe} src={photoMe} alt="photoMe"/>
+        </Fade>
       </div>
     </div>
   );
